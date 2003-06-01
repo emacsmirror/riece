d3e5f6ce6488690c232e5d2e8208517b251c6f4b
Daiki Ueno <ueno@unixuser.org>
2003-06-01 07:31:30 +0000

    * riece-rdcc.el: New add-on. * Makefile.am (EXTRA_DIST): Add riece-rdcc.el

--- lisp/riece-rdcc.el
+++ lisp/riece-rdcc.el
@@ -1,4 +1,4 @@
-;;; riece-ndcc.el --- elisp native DCC add-on
+;;; riece-rdcc.el --- ruby implementation of DCC add-on
 ;; Copyright (C) 1998-2003 Daiki Ueno
 
 ;; Author: Daiki Ueno <ueno@unixuser.org>
@@ -23,128 +23,130 @@
 
 ;;; Code:
 
-(require 'calc)
-
-(defgroup riece-ndcc nil
-  "Elisp native DCC implementation"
+(defgroup riece-rdcc nil
+  "DCC implementation using ruby"
   :prefix "riece-"
   :group 'riece)
 
-(defcustom riece-ndcc-server-address "127.0.0.1"
+(defcustom riece-rdcc-server-address "127.0.0.1"
   "Local address of the DCC server.
 Only used for sending files."
   :type 'vector
-  :group 'riece-ndcc)
+  :group 'riece-rdcc)
 
-(defvar riece-ndcc-requests nil)
+(defvar riece-rdcc-requests nil)
 
-(defvar riece-ndcc-request-user nil)
-(defvar riece-ndcc-request-size nil)
+(defvar riece-rdcc-request-user nil)
+(defvar riece-rdcc-request-file nil)
+(defvar riece-rdcc-request-size nil)
 
-(defun riece-ndcc-encode-address (address)
-  (unless (string-match
-	   "^\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)$"
-	   address)
-    (error "% is not an IP address" address))
-  (let ((calc-number-radix 10))
-    (calc-eval (format "%s * (2 ** 24) + %s * (2 **16) + %s * (2 ** 8) + %s"
-		       (match-string 1 address)
-		       (match-string 2 address)
-		       (match-string 3 address)
-		       (match-string 4 address)))))
+(defun riece-rdcc-server-filter (process input)
+  (save-excursion
+    (set-buffer (process-buffer process))
+    (goto-char (point-max))
+    (insert input)
+    (goto-char (point-min))
+    (while (and (not (eobp))
+		(looking-at "\\([0-9]+\\)\n"))
+      (message "Sending %s...(%s/%d)"
+	       riece-rdcc-request-file
+	       (match-string 1) riece-rdcc-request-size)
+      (forward-line))
+    (unless (eobp)
+      (delete-region (point-min) (point)))))
 
-(defun riece-ndcc-decode-address (address)
-  (format "%d.%d.%d.%d"
-	  (floor (string-to-number
-		  (calc-eval (format "(%s / (2 ** 24)) %% 256" address))))
-	  (floor (string-to-number
-		  (calc-eval (format "(%s / (2 ** 16)) %% 256" address))))
-	  (floor (string-to-number
-		  (calc-eval (format "(%s / (2 ** 8)) %% 256" address))))
-	  (floor (string-to-number
-		  (calc-eval (format "%s %% 256" address))))))
-
-(defun riece-ndcc-server-sentinel (process status)
-  (when (string-match "^open from " status)
-    (let ((parent-name
-	   (if (string-match " <[^>]+>$" (process-name process))
-	       (substring (process-name process) 0 (match-beginning 0)))))
-      (save-excursion
-	(set-buffer (process-buffer (get-process parent-name)))
-	(delete-process parent-name)
-	(goto-char (point-min))
-	(while (not (eobp))
-	  (process-send-region process
-			       (point)
-			       (goto-char (min (+ 1024 (point)) (point-max))))
-	  (message "Sending %s...(%d/%d)"
-		   (buffer-file-name) (1- (point)) (buffer-size)))
-	(message "Sending %s...done"
-		 (buffer-file-name))))
-    (kill-buffer (process-buffer process))
-    (delete-process process)))
+(defun riece-rdcc-server-sentinel (process status)
+  (kill-buffer (process-buffer process))
+  (delete-process process))
 
 (defun riece-command-dcc-send (user file)
   (interactive
    (let ((completion-ignore-case t))
-     (unless riece-ndcc-server-address
-       (error "Set riece-ndcc-server-address to your host"))
+     (unless riece-rdcc-server-address
+       (error "Set riece-rdcc-server-address to your host"))
      (list (completing-read
 	    "User: "
 	    (mapcar #'list (riece-get-users-on-server)))
 	   (expand-file-name (read-file-name "File: ")))))
-  (let* (selective-display
-	 (coding-system-for-read 'binary)
-	 format-alist
-	 jka-compr-compression-info-list
-	 (buffer (find-file-noselect file))
-	 process)
-    (with-current-buffer buffer		;To throw an error when the
-      (setq buffer-read-only t))	;process has input.
-    (setq process (make-network-process :name "DCC" :buffer buffer
-					:host riece-ndcc-server-address
-					:server t :service t
-					:coding 'binary
-					:sentinel 'riece-ndcc-server-sentinel))
-    (riece-send-string
-     (format "PRIVMSG %s :\1DCC SEND %s %s %d %d\1\r\n"
-	     user (file-name-nondirectory file)
-	     (riece-ndcc-encode-address riece-ndcc-server-address)
-	     (nth 1 (process-contact process))
-	     (nth 7 (file-attributes file))))))
+  (let ((process
+	 (start-process "DCC" " *DCC*" "ruby" "-rsocket")))
+    (process-send-string process (concat "\
+server = TCPServer.new('" riece-rdcc-server-address "', 0)
+puts(\"#{server.addr[3].split(/\\./).collect{|c| c.to_i}.pack('cccc').unpack('N')[0]} #{server.addr[1]}\")
+session = server.accept
+if session
+  total = 0
+  File.open('" file "') {|file|
+    bytes = file.read(1024)
+    total += bytes.length
+    puts(\"#{total}\")
+    session.write(bytes)
+  }
+  session.close
+end
+"))
+    (process-send-eof process)
+    (save-excursion
+      (set-buffer (process-buffer process))
+      (while (and (eq (process-status process) 'run)
+		  (progn
+		    (goto-char (point-min))
+		    (not (looking-at "\\([0-9]+\\) \\([0-9]+\\)"))))
+	(accept-process-output))
+      (if (eq (process-status process) 'run)
+	  (let ((address (match-string 1))
+		(port (match-string 2)))
+	    (erase-buffer)
+	    (make-local-variable 'riece-rdcc-request-size)
+	    (setq riece-rdcc-request-file file
+		  riece-rdcc-request-size (nth 7 (file-attributes file)))
+	    (set-buffer-modified-p nil)
+	    (set-process-filter process #'riece-rdcc-server-filter)
+	    (set-process-sentinel process #'riece-rdcc-server-sentinel)
+	    (riece-send-string
+	     (format "PRIVMSG %s :\1DCC SEND %s %s %s %d\1\r\n"
+		     user (file-name-nondirectory file)
+		     address port
+		     riece-rdcc-request-size)))))))
 
-(defun riece-ndcc-filter (process input)
+(defun riece-rdcc-filter (process input)
   (save-excursion
     (set-buffer (process-buffer process))
     (goto-char (point-max))
     (insert input)
     (message "Receiving %s from %s...(%d/%d)"
 	     (file-name-nondirectory buffer-file-name)
-	     riece-ndcc-request-user
+	     riece-rdcc-request-user
 	     (1- (point))
-	     riece-ndcc-request-size)))
+	     riece-rdcc-request-size)))
 
-(defun riece-ndcc-sentinel (process status)
+(defun riece-rdcc-sentinel (process status)
   (save-excursion
     (set-buffer (process-buffer process))
-    (unless (= (buffer-size) riece-ndcc-request-size)
+    (unless (= (buffer-size) riece-rdcc-request-size)
       (error "Premature end of file"))
     (message "Receiving %s from %s...done"
 	     (file-name-nondirectory buffer-file-name)
-	     riece-ndcc-request-user)
+	     riece-rdcc-request-user)
     (let ((coding-system-for-write 'binary))
       (save-buffer))))
+
+(defun riece-rdcc-decode-address (address)
+  (with-temp-buffer
+    (call-process "ruby" nil t nil "-e" (concat "\
+puts(\"#{" address " >> 24 & 0xFF}.#{" address " >> 16 & 0xFF}.#{" address " >> 8 & 0xFF}.#{" address " & 0xFF}\")"))
+    (buffer-substring (point-min) (1- (point-max)))))
 
 (defun riece-command-dcc-receive (request file)
   (interactive
    (progn
-     (unless riece-ndcc-requests
+     (unless riece-rdcc-requests
        (error "No request"))
      (list
-      (if (= (length riece-ndcc-requests) 1)
-	  (car riece-ndcc-requests)
+      (if (= (length riece-rdcc-requests) 1)
+	  (car riece-rdcc-requests)
 	(with-output-to-temp-buffer "*Help*"
-	  (let ((requests riece-ndcc-requests)
+	  (let ((requests riece-rdcc-requests)
 		(index 1))
 	    (while requests
 	      (princ (format "%2d: %s %s (%d bytes)\n"
@@ -158,29 +160,29 @@ Only used for sending files."
 	  (unless (string-match "^[0-9]+$" number)
 	    (error "Not a number"))
 	  (if (or (> (setq number (string-to-number number))
-		     (length riece-ndcc-requests))
+		     (length riece-rdcc-requests))
 		  (< number 1))
 	      (error "Invalid number"))
-	  (nth (1- number) riece-ndcc-requests)))
+	  (nth (1- number) riece-rdcc-requests)))
       (expand-file-name (read-file-name "Save as: ")))))
   (let* (selective-display
 	 (coding-system-for-read 'binary)
 	 (coding-system-for-write 'binary)
 	 (process (open-network-stream
 		   "DCC" " *DCC*"
-		   (riece-ndcc-decode-address (nth 2 request))
+		   (riece-rdcc-decode-address (nth 2 request))
 		   (nth 3 request))))
     (setq riece-rdcc-requests (delq request riece-rdcc-requests))
     (with-current-buffer (process-buffer process)
       (set-buffer-multibyte nil)
       (buffer-disable-undo)
       (setq buffer-file-name file)
-      (make-local-variable 'riece-ndcc-request-user)
-      (setq riece-ndcc-request-user (car request))
-      (make-local-variable 'riece-ndcc-request-size)
-      (setq riece-ndcc-request-size (nth 4 request)))
-    (set-process-filter process #'riece-ndcc-filter)
-    (set-process-sentinel process #'riece-ndcc-sentinel)))
+      (make-local-variable 'riece-rdcc-request-user)
+      (setq riece-rdcc-request-user (car request))
+      (make-local-variable 'riece-rdcc-request-size)
+      (setq riece-rdcc-request-size (nth 4 request)))
+    (set-process-filter process #'riece-rdcc-filter)
+    (set-process-sentinel process #'riece-rdcc-sentinel)))
 
 (defun riece-handle-dcc-request (prefix target message)
   (let ((case-fold-search t))
@@ -196,9 +198,9 @@ Only used for sending files."
 			      (riece-make-identity target)
 			      riece-channel-buffer-alist))))
 	    (user (riece-prefix-nickname prefix)))
-	(setq riece-ndcc-requests
+	(setq riece-rdcc-requests
 	      (cons (list user file address port size)
-		    riece-ndcc-requests))
+		    riece-rdcc-requests))
 	(riece-insert-change buffer (format "DCC SEND from %s\n" user))
 	(riece-insert-change
 	 (if (and riece-channel-buffer-mode
@@ -215,15 +217,15 @@ Only used for sending files."
 	  "\n")))
       t)))
 
-(defun riece-ndcc-requires ()
+(defun riece-rdcc-requires ()
   '(riece-ctcp))
 
 (defvar riece-dialogue-mode-map)
-(defun riece-ndcc-insinuate ()
+(defun riece-rdcc-insinuate ()
   (add-hook 'riece-ctcp-dcc-request-hook 'riece-handle-dcc-request)
   (define-key riece-dialogue-mode-map "\C-ds" 'riece-command-dcc-send)
   (define-key riece-dialogue-mode-map "\C-dr" 'riece-command-dcc-receive))
 
-(provide 'riece-ndcc)
+(provide 'riece-rdcc)
 
-;;; riece-ndcc.el ends here
+;;; riece-rdcc.el ends here
