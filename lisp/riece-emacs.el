f29c0a2fa1facec5f3f26fe7e0f93d726f08f5cf
Daiki Ueno <ueno@unixuser.org>
2004-12-14 02:12:56 +0000

    * riece-emacs.el (riece-normalize-modeline-string): Don't concatenate elements in modeline string.

--- lisp/riece-emacs.el
+++ lisp/riece-emacs.el
@@ -105,7 +105,7 @@
 
 (defun riece-normalize-modeline-string (string)
   (if (listp string)
-      (apply #'concat (riece-normalize-modeline-string-1 string))
+      (riece-normalize-modeline-string-1 string)
     string))
 
 (defun riece-put-text-property-nonsticky (start end prop value
