ea26c9283b9a4aff978f21cfeb519a0daa7fc213
Daiki Ueno <ueno@unixuser.org>
2005-08-18 08:18:58 +0000

    Fixed.

--- lisp/riece-addon.el
+++ lisp/riece-addon.el
@@ -175,7 +175,7 @@
 	(setq pointer (cdr pointer)))
       (setq queue (cdr queue)))
     (if dependencies
-	(error "Circular add-on dependency found"))
+	(error "Circular add-on dependency found: %S" dependency))
     (nreverse addons)))
 
 (defun riece-resolve-addons (addons)
