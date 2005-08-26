e6c745cfdf9afe9d9ad4de3de5e3a4562133282f
Daiki Ueno <ueno@unixuser.org>
2005-08-26 07:49:11 +0000

    Use remprop instead of (put ... nil)

--- lisp/riece-addon.el
+++ lisp/riece-addon.el
@@ -229,7 +229,7 @@
 			 #'ignore))
 	    (setq riece-addon-dependencies
 		  (delq entry riece-addon-dependencies))
-	    (put addon 'riece-addon-insinuated nil)
+	    (remoprop addon 'riece-addon-insinuated)
 	    (setq riece-addons (delq addon riece-addons))))
       (if verbose
 	  (message "Add-on %S is uninstalled" addon)))))
