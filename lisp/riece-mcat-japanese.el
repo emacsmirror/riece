67e5c272c17c30814ae95db70b696c0d09908b9f
Daiki Ueno <ueno@unixuser.org>
2007-01-19 06:38:20 +0000

    Added header.

--- lisp/riece-mcat-japanese.el
+++ lisp/riece-mcat-japanese.el
@@ -1,4 +1,30 @@
-;;; -*- coding: iso-2022-jp -*-
+;;; riece-mcat.el --- message catalog for Japanese -*- coding: iso-2022-jp -*-
+;; Copyright (C) 2007 Daiki Ueno
+
+;; Author: Daiki Ueno <ueno@unixuser.org>
+
+;; This file is part of Riece.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; To update riece-mcat-japanese-alist, `make update-mcat'.
+
+;;; Code:
 
 (defconst riece-mcat-japanese-alist
   '(("Sending QUIT...done" . "QUIT を送信しています...完了")
@@ -23,3 +49,5 @@
     ("Really quit IRC? " . "本当に IRC をやめますか? ")))
 
 (provide 'riece-mcat-japanese)
+
+;;; riece-mcat-japanese.el ends here
--- lisp/riece-mcat.el
+++ lisp/riece-mcat.el
@@ -1,3 +1,27 @@
+;;; riece-mcat.el --- message catalog
+;; Copyright (C) 2007 Daiki Ueno
+
+;; Author: Daiki Ueno <ueno@unixuser.org>
+
+;; This file is part of Riece.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (defvar riece-mcat-alist
   '(("Japanese" . riece-mcat-japanese)))
 
@@ -70,3 +94,5 @@
       (save-buffer))))
 
 (provide 'riece-mcat)
+
+;;; riece-mcat.el ends here
