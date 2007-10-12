42e659f14514b32891acdc885c1c87612b990c8f
Daiki Ueno <ueno@unixuser.org>
2007-10-12 12:37:31 +0000

    * riece-twitter.el (riece-twitter-update): Don't send extra headers.

--- lisp/riece-twitter.el
+++ lisp/riece-twitter.el
@@ -70,11 +70,11 @@
   (let ((process
 	 (start-process
 	  "curl" nil "curl"
-	  "-H" "X-Twitter-Client: Riece"
-	  "-H" (concat "X-Twitter-Client-Version: " riece-version-number)
-	  "-H" "X-Twitter-Client-URL: http://riece.nongnu.org/twitter.xml"
+;;	  "-H" "X-Twitter-Client: Riece"
+;;	  "-H" (concat "X-Twitter-Client-Version: " riece-version-number)
+;;	  "-H" "X-Twitter-Client-URL: http://riece.nongnu.org/twitter.xml"
 	  "-u" riece-twitter-credential
-	  "-d" "source=riece"
+;;	  "-d" "source=riece"
 	  "-d" (concat "status="
 		       (riece-twitter-escape-string
 			(encode-coding-string status 'utf-8)))
